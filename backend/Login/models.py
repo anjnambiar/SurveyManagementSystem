c7821d0b1fa77028021064e4502ee04d04b135ce
anjnambiar <anjnambiar@gmail.com>
2025-01-04 16:46:25 -0500

    custom model and serializer

--- backend/Login/models.py
+++ backend/Login/models.py
@@ -2,20 +2,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
-class User(models.Model) :
+class CustomUser(AbstractBaseUser) :
+    email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
     contactNum = models.IntegerField(max_length=20)
     password = models.CharField(max_length=50)
-    date_of_birth = models.DateField()
     is_Admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS=["name","contactNum","password","date_of_birth"]
+    REQUIRED_FIELDS=["name","contactNum","password"]
     objects = models.CustomUserManager()
 
+    def __str__(self):
+        return self.email
+
+
+
 class CustomUserManager(BaseUserManager) :
+    """Custom user model manager where email is the unique
+    identifier for authentication instead of username."""
+
     def createUser(self, email, password = None, **extra_fields) :
+        """Create and save a user with the given email and password."""
         if not email :
             return ValueError("Email is required")
         email = self.normalize_email(email)
@@ -25,5 +33,6 @@ class CustomUserManager(BaseUserManager) :
         return user
 
     def createSuperUser(self, email, password = None, **extra_fields) :
+        """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault('is_superUser', True)
-        return self.creareUser(email, password, **extra_fields)
+        return self.createUser(email, password, **extra_fields)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "users.CustomUser"
 
 # Application definition
 
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Login',
+    'rest_framework',
+    'Login'
 ]
 
 MIDDLEWARE = [
