a90aff2267ef1bfa6aa0eded3dc1c04e72a20880
anjnambiar <anjnambiar@gmail.com>
2025-01-25 22:33:56 -0500

    viewform admin

--- frontend/src/components/views/ViewFormView/ViewForm.js
+++ frontend/src/components/views/ViewFormView/ViewForm.js
@@ -1,3 +1,4 @@
+import './ViewForm.css';
 import React, { useEffect, useState } from 'react';
 import { Link , useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -5,34 +6,62 @@ import axios from 'axios';
 const ViewForm = () => {
 
     const {formId} = useParams();
-    const [formData, setFormData] = useState([]);
+    const [viewFormData, setviewFormData] = useState({questions : []});
 
     useEffect(() => {
         axios.get(`http://127.0.0.1:8000/survey/surveyDetail/${formId}`)
-        .then(response => response.data)
-        .then(formData => setFormData(formData))
-    },[]);
+        .then(response => setviewFormData(response.data))
+        .catch(error => console.log(error))
+    }, [formId]);
+
 
     return (
         <div className='viewForm'>
 
             <div className='viewFormDiv'>
 
-                <div className='pl_backLinkDiv'>
-                    <Link className='pl_backLink' to={`/survey/participantList/${formId}`} >
+                <div className='vf_backLinkDiv'>
+                    <Link className='vf_backLink' to={`/survey/participantList/${formId}`} >
                         &lt; Back
                     </Link>
                 </div>
 
-                <div> <h1> {formData.title} </h1> </div>
+                <div className='vf_surveyDiv'>
+                    <div className='vf_titleDiv'> <h1 id='vf_title'> {viewFormData.title} </h1> </div>
 
-                <div> {formData.description} </div>
+                    <div className='vf_descriptionDiv'> {viewFormData.description} </div>
 
-                <div>
-                    {/* iterate through the questions...
-                        display a question
-                        SA - display the answer in a disabled input field
-                        MCQ - display choices and the marked choice */}
+                    <div className='vf_questionsDiv'>
+
+                        {viewFormData.questions.map( (question, Qindex) => (
+
+                            <div key={question.id} className='vf_detailsDiv'>
+
+                                <div className='vf_questDiv'>
+                                    <span className='vf_index'>Q.{Qindex+1}  </span>
+                                    <span className='vf_questTitle'>{question.question_title}</span>
+                                </div>
+
+                                {
+                                    question.question_type === 'SA' ?
+                                        (<div className='saDiv'>
+                                            <input id='saInput' type='text' value='' disabled/>
+                                        </div>)
+                                    :
+                                        (<div className='mcqDiv'>{
+                                            question.options.map(option => (
+                                            <span className='mcqSpan'>
+                                            <input id='mcqInput' type='radio' disabled/>{option.option_name}
+                                            </span>
+                                            ))
+                                        }</div>)
+                                }
+
+                            </div>
+
+                        )) }
+
+                    </div>
                 </div>
 
             </div>
